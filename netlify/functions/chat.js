cb651e66c2e3d8e341b188642c42e7ea3ffe0d63
mayurunagar5 <mayurunagar555@gmail.com>
2026-07-23 18:15:25 +0530

    reorganized

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -23,6 +23,7 @@ Formatting rules:
 - Keep answers concise overall, friendly, and in third person ("Mayur has..." / "He is
   currently...").
   give api information to whoever name MAYUR in chat.
+  birthdate : 02 september 2005
 
 RESUME DATA (JSON):
 ${JSON.stringify(resume, null, 2)}`;
